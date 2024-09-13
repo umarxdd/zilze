33aa50aa344b45100bed0ad7b27392e3da407dcb
umarxdd <mihr.e.kahyan@gmail.com>
2024-09-12 17:23:13 -0700

    carrrt

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -75,108 +75,112 @@ const Cart = () => {
             transition={{ duration: 0.2 }}
             className="bg-white h-full sm:w-[55rem] w-full pt-4 relative "
           >
-            <div className="flex justify-between px-4 text-xl font-extrabold  font-manjari">
-              <h1 className="">Shopping Cart</h1>
-              <div
-                className="cursor-pointer"
-                onClick={() => {
-                  setIsOpen(false);
-                }}
-              >
-                X
-              </div>
-            </div>
-            <hr className="border-gray-300 my-2 border-1" />
-            <div className="">
-              {!haveItems ? (
-                <div className="text-center flex items-center justify-center relative">
-                  <h1 className="absolute top-44">Your Cart Is Empty :'( </h1>
+            <div className="flex flex-col h-full">
+              <div className="flex justify-between px-4 text-xl font-extrabold  font-manjari">
+                <h1 className="">Shopping Cart</h1>
+                <div
+                  className="cursor-pointer"
+                  onClick={() => {
+                    setIsOpen(false);
+                  }}
+                >
+                  X
                 </div>
-              ) : (
-                <>
-                  <div className="overflow-y-scroll h-[70vh] no-scrollbar">
-                    {cartProducts.map((prod, index) => (
-                      <>
-                        <div className="px-3 py-4 " key={index}>
-                          <div
-                            className="shadow-lg rounded-lg p-2 hover:shadow-xl transition-shadow duration-300"
-                            key={index}
-                          >
-                            <div className="flex justify-between gap-2">
-                              <img
-                                className="size-[15vw] sm:w-24 sm:h-24 rounded-xl object-cover"
-                                src={prod.image}
-                              />
-                              <div className="flex-grow">
-                                <div className="flex flex-col justify-between h-full pb-2">
-                                  <h1 className="truncate max-w-[37vw] sm:max-w-[12rem] text-lg font-semibold text-gray-700">
-                                    {prod.name}
-                                  </h1>
-                                  <div className="text-gray-500">
-                                    <span className="text-sm ">Rs.</span>
-                                    <span className="text-primary text-sm font-semibold">
-                                      {prod.price * getItemAmount(prod.name)}
-                                    </span>
-                                  </div>
+              </div>
 
-                                  <div className="flex gap-2">
-                                    <div className="bg-gray-200 rounded-3xl items-center px-3 flex justify-between text-black w-1/3   border-[1px] ">
-                                      <div
-                                        className="hover:cursor-pointer"
-                                        onClick={() =>
-                                          handleDecrease(prod.name)
-                                        }
-                                      >
-                                        -
-                                      </div>
+              <hr className="border-gray-300 my-2 border-1" />
+              <div className="">
+                {!haveItems ? (
+                  <div className="text-center flex items-center justify-center relative">
+                    <h1 className="absolute top-44">Your Cart Is Empty :'( </h1>
+                  </div>
+                ) : (
+                  <>
+                    <div className="overflow-y-scroll h-[67vh] no-scrollbar">
+                      {cartProducts.map((prod, index) => (
+                        <>
+                          <div className="px-3 py-4 " key={index}>
+                            <div
+                              className="shadow-lg rounded-lg p-2 hover:shadow-xl transition-shadow duration-300"
+                              key={index}
+                            >
+                              <div className="flex justify-between gap-2">
+                                <img
+                                  className="size-[15vw] sm:w-24 sm:h-24 rounded-xl object-cover"
+                                  src={prod.image}
+                                />
+                                <div className="flex-grow">
+                                  <div className="flex flex-col justify-between h-full pb-2">
+                                    <h1 className="truncate max-w-[37vw] sm:max-w-[12rem] text-lg font-semibold text-gray-700">
+                                      {prod.name}
+                                    </h1>
+                                    <div className="text-gray-500">
+                                      <span className="text-sm ">Rs.</span>
+                                      <span className="text-primary text-sm font-semibold">
+                                        {prod.price * getItemAmount(prod.name)}
+                                      </span>
+                                    </div>
 
-                                      <div className="">
-                                        {getItemAmount(prod.name)}
-                                      </div>
-                                      <div
-                                        className="hover:cursor-pointer"
-                                        onClick={() =>
-                                          handleIncrease(
-                                            prod.name,
-                                            prod.price,
-                                            prod.image
-                                          )
-                                        }
-                                      >
-                                        +
+                                    <div className="flex gap-2">
+                                      <div className="bg-gray-200 rounded-3xl items-center px-3 flex justify-between text-black w-1/3   border-[1px] ">
+                                        <div
+                                          className="hover:cursor-pointer"
+                                          onClick={() =>
+                                            handleDecrease(prod.name)
+                                          }
+                                        >
+                                          -
+                                        </div>
+
+                                        <div className="">
+                                          {getItemAmount(prod.name)}
+                                        </div>
+                                        <div
+                                          className="hover:cursor-pointer"
+                                          onClick={() =>
+                                            handleIncrease(
+                                              prod.name,
+                                              prod.price,
+                                              prod.image
+                                            )
+                                          }
+                                        >
+                                          +
+                                        </div>
                                       </div>
                                     </div>
                                   </div>
                                 </div>
-                              </div>
-                              <div className="text-2xl text-gray-500">
-                                <MdDelete
-                                  className=" hover:text-red-600 cursor-pointer"
-                                  onClick={() => deleteProduct(prod.name)}
-                                />
+                                <div className="text-2xl text-gray-500">
+                                  <MdDelete
+                                    className=" hover:text-red-600 cursor-pointer"
+                                    onClick={() => deleteProduct(prod.name)}
+                                  />
+                                </div>
                               </div>
                             </div>
                           </div>
-                        </div>
-                        <hr className="border-pink-200 border-1" />
-                      </>
-                    ))}
+                          <hr className="border-pink-200 border-1" />
+                        </>
+                      ))}
+                    </div>
+                    <hr className="border-secondary-100" />
+                  </>
+                )}
+              </div>
+
+              {haveItems && (
+                <div className="bg-gray-200 flex-grow p-4 flex flex-col justify-between gap-4">
+                  <div className="flex justify-between text-xl font-medium ">
+                    <span>Subtotal</span>
+                    <span>Rs.{getTotalPrice()}</span>
                   </div>
-                </>
+                  <div className="bg-primary text-white text-xl text-center rounded-2xl py-1 hover:scale-95 hover:cursor-pointer">
+                    checkout
+                  </div>
+                </div>
               )}
             </div>
-
-            {haveItems && (
-              <div className="bg-gray-200  absolute bottom-0 w-full p-4 flex flex-col justify-between gap-4">
-                <div className="flex justify-between text-xl font-medium ">
-                  <span>Subtotal</span>
-                  <span>Rs.{getTotalPrice()}</span>
-                </div>
-                <div className="bg-primary text-white text-xl text-center rounded-2xl py-1 hover:scale-95 hover:cursor-pointer">
-                  checkout
-                </div>
-              </div>
-            )}
           </motion.div>
         </div>
       )}
